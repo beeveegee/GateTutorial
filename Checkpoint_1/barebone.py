b65b364f91c0b1bcad13d01152cb22602966fc28
Brittany Van Grouw <brittanyvangrouw@gmail.com>
2015-08-30 09:38:38 -0600

    Checkpoint Barebone beginning

---
+++ Checkpoint_1/barebone.py
@@ -0,0 +1,19 @@
+#GateTutorial
+#http://samolds.github.io/gate/guide/hello_world.html#begin
+
+#barebone.py
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.write("Hello, World!")
+
+class SecondPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.write("New Page!")
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage),
+  ('/two', SecondPage),
+] debug=True)
